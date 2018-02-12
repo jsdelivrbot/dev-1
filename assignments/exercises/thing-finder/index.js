c4478fd3a00bd1474b05246c87d820157e44de92
Matthew Adams <matthewdadams92@gmail.com>
2018-02-11 22:15:26 -0500

    updated react app and express exercise

---
+++ assignments/exercises/thing-finder/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const uuid = require('uuid');
+
+const fruitsRoute = require('./routes/fruits.js');
+
+let fruits = require('./fruitsdatabase.js');
+
+const app = express();
+let port = 5050;
+
+app.use(bodyParser.json());
+
+app.use('/fruits', fruitsRoute);
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
+
